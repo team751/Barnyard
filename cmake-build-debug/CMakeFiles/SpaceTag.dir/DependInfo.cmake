590caed2ee190981f7c32f69a1c7a255a929f11d
Bobby Youstra <bobby@youstra.com>
2018-10-15 07:12:02 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SpaceTag.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bobby/CLionProjects/SpaceTag/main.cpp" "/home/bobby/CLionProjects/SpaceTag/cmake-build-debug/CMakeFiles/SpaceTag.dir/main.cpp.o"
+  "/home/bobby/CLionProjects/SpaceTag/sptag/nfc/taguidextractor.cpp" "/home/bobby/CLionProjects/SpaceTag/cmake-build-debug/CMakeFiles/SpaceTag.dir/sptag/nfc/taguidextractor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
