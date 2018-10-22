81c798e89137e00c53e082aa0107287b66329d6f
Bobby Youstra <bobby@youstra.com>
2018-10-22 08:10:47 -0700

    Syncing commit

--- sptaggui/sptag/sheets/UidSheetInfoModifier.py
+++ sptaggui/sptag/sheets/UidSheetInfoModifier.py
@@ -1,1 +1,17 @@
+from oauth2client import file
+
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+SPREADSHEET_ID = "1zRSYqFLEEHLTDiMwv_tjmZ2aUK3V4LZ9E4OVBDFX_OI"
+
+class UidSheetInfoModifier:
+    _credentials = None
+
+
+    def __init__(self):
+        cred_file = file.Storage("credentials.json")
+        self._credentials = cred_file.get()
+
+        if not self._credentials or self._credentials.invalid:
+            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+            creds = tools.run_flow(flow, store)
