606497e00668d0fab8f86c00cd7fa8b004f88b2b
Bobby Youstra <bobbyyoustra@protonmail.com>
2019-05-16 15:50:06 -0700

    Worked on getting labels for text boxes in TagEditorScreen

--- sptaggui/sptag/main.py
+++ sptaggui/sptag/main.py
@@ -2,13 +2,13 @@ import tkinter
 import os
 
 from sptag.gui.TagInfoScreen import TagInfoScreen
-from sptag.gui.TagEditorScreen import TagEditorScreen
+#from sptag.gui.TagEditorScreen import TagEditorScreen
 
 def main():
     window = tkinter.Tk()
-    #tag_info_screen = TagInfoScreen(window)
-    #tag_info_screen.start()
-    tag_editor_screen = TagEditorScreen(window)
+    tag_info_screen = TagInfoScreen(window)
+    tag_info_screen.start()
+    #tag_editor_screen = TagEditorScreen(window)
 
     window.mainloop()
 
